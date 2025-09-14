cc1cc81dabf85354bc23c163a862a5f081f1d6ee
bxnsir <a0980306278@gmail.com>
2025-09-14 21:07:53 +0800

    2025 / 08 / 06 新學習程式碼: 宣告浮點數變數 float  、 %f 新學習內容: 宣告浮點數變數 float 並命名儲存浮點數值 ，再用 %f 將其顯示出來

---
+++ 格式對應字範例.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+	int num=16; // 俱计跑计 num结ぉ计 16 
+	float score=98.5;  // 疊翴计跑计结ぉ嘿 score 
+	printf("Number:%d\tscore:%f\n", num, score); //俱计跑计籔疊翴计跑计 
+}
