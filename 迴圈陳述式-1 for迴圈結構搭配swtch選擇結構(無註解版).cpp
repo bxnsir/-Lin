fa77321c763c2b319a7d7a754369416315e29720
bxnsir <a0980306278@gmail.com>
2025-10-08 22:17:30 +0800

    2025 / 10 / 08 新學習程式碼:  無 新學習內容: 透過 for 迴圈與 switch 判斷式來顯示迴圈的次數

---
+++ 迴圈陳述式-1 for迴圈結構搭配swtch選擇結構(無註解版).cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+    int j;
+
+    for ( j = 1; j <= 9; j++)
+    {
+        switch(j)
+        {
+        case 1:
+         printf("第一次迴圈:%d\n",j*j);
+         break;
+         
+        case 2:
+         printf("第二次迴圈:%d\n",j*j);
+         break;
+         
+        case 3:
+         printf("第三次迴圈:%d\n",j*j);
+         break;
+         
+        case 4:
+         printf("第四次迴圈:%d\n",j*j);
+         break;
+         
+        case 5:
+         printf("第五次迴圈:%d\n",j*j);
+         break;
+         
+        case 6:
+         printf("第六次迴圈:%d\n",j*j);
+         break;
+         
+        case 7:
+         printf("第七次迴圈:%d\n",j*j);
+         break;
+         
+        case 8:
+         printf("第八次迴圈:%d\n",j*j);
+         break;
+		 
+		case 9:
+         printf("第九次迴圈:%d\n",j*j);
+         break;  
+        }
+        
+    }
+    
+    return 0;
+}
