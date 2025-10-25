3524c72db28f875c8543a91e6a69d84d30524f9b
bxnsir <a0980306278@gmail.com>
2025-10-25 22:01:00 +0800

    2025 / 10 / 25 新學習程式碼:  for-巢狀迴圈結構 新學習內容:  使用兩個 for 迴圈組成最外層迴圈與最內層迴圈的巢狀迴圈結構

---
+++ 迴圈之蜂巢結構(無註解版).cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for(i=1; i<=5; i+=2)
+    {
+         (j=2; j<=6; j+=2)
+        {
+            printf("(%d,%d)\t", i, j);
+        }
+         ntf("\n");
+    }
+    
+return 0;    
+}
